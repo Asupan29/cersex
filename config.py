e063b709e015cd3e039b3141de68467989539f69
Asupan29 <138056817+Asupan29@users.noreply.github.com>
2023-08-26 20:53:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001673468177"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
-FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001510659633"))
+FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "-1001721038526"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 FORCE_SUB_5 = int(os.environ.get("FORCE_SUB_5", "0"))
 FORCE_SUB_6 = int(os.environ.get("FORCE_SUB_6", "0"))
